cbb2e63edf4ef1cd215bf35a3910714b092b7ca8
Dannywhodunnit <dannywhodunnit@gmail.com>
2019-02-26 18:41:02 -0500

    First push

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,41 @@
+class CharactersController < ApplicationController
+	def index
+		@characters = Character.all
+	end
+
+	def show
+		@character = Character.find(params[:id])
+	end
+
+	def new
+		@character = Character.new
+	end  
+
+	def create
+		params.require(:character).permit!
+		@character = Character.new(params[:book])
+		if @character.save
+			redirect_to character_path(@character)
+		end
+	end  
+
+	def edit
+		@character = Character.find(params[:id])
+	end  
+
+	def update
+		@character = Character.find(params[:id])
+		if @character.update(book_params)
+			redirect_to @character
+		else
+			render 'edit'
+		end  
+	end  
+
+	def character_params
+		params.require(:character).permit!
+	end 
+
+end 
+
+	
